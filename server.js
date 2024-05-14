f52bbf565879d11e5273d967e57daca8140a743e
Opeyemi <opeadetola@gmail.com>
2024-05-14 00:19:38 -0400

    Initialized Node server with express.js --devOpe --

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+//Initialize Express App
+const app = express();
+
+//Start Server
+app.listen(() => {
+  console.log("Server is listening on port 5000");
+});
