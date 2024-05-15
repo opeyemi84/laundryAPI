ba1bcf3246863f12ae0e2a3516792e7d4e9bc8e6
fabian nwosu <opeadetola@gmail.com>
2024-05-14 22:31:58 -0400

    server changes --devOpe--

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const bodyparser = require('body-parser')
 
 //Initialize Express App
 const app = express();
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Home</h1>')
+
+})
 
 //Start Server
 app.listen(5000, () => {
