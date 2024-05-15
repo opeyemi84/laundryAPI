15665077bc8561e2351e098938388749bc59484a
Opeyemi <opeadetola@gmail.com>
2024-05-15 11:03:01 -0400

    upd

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express");
 //Initialize Express App
 const app = express();
 
+//Home Route
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the Twins Laundry Ventures</h1>");
+});
+
 //Start Server
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
